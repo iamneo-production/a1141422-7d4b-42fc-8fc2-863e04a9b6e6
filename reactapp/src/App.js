2f5c1574e2a13cd27c689968906e79805800623c
Traxxgod <prakhar123shukla@gmail.com>
2023-07-07 11:34:52 +0000

    Header by Prakhar

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import './App.css';
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
+import ListEmployeeComponent from './components/ListEmployeeComponent'
+import AddEmployeeComponent from './components/AddEmployeeComponent';
+import ListRepairComponent from './components/ListRepairComponent';
+import AddRepairComponent from './components/AddRepairComponent';
+import Sidebar from './components/Sidebar';
+import AdminMainPage from './components/AdminMainPage';
+import Home from './pages/home/Home.js'
+import AddCustomerComponent from './components/AddCustomerComponent';
+import ListCustomerComponent from './components/ListCustomerComponent';
+import ListDeviceComponent from './components/ListDeviceComponent';
+import AddDeviceComponent from './components/AddDeviceComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <Router>
+    <HeaderComponent/>
+    <Sidebar></Sidebar>
+    <Routes>
+    <Route path="/" element={<Home/>}> 
+      </Route>
+      <Route  path = "/" element = {<Home/>}></Route>
+      <Route path="/admin" element={<AdminMainPage/>}></Route>
+      <Route path = "/employees" element = {<ListEmployeeComponent/>}></Route>
+      <Route path = "/add-employee" element = {<AddEmployeeComponent/>} ></Route>
+      <Route path="/edit-employee/:id" element={<AddEmployeeComponent/>}></Route>
+      <Route path = "/repairs" element = {<ListRepairComponent/>}></Route>
+      <Route path = "/add-repair" element = {<AddRepairComponent/>} ></Route>
+      <Route path="/edit-repair/:id" element={<AddRepairComponent/>}></Route>
+      <Route path = "/customers" element = {<ListCustomerComponent/>}></Route>
+      <Route path = "/add-customer" element = {<AddCustomerComponent/>} ></Route>
+      <Route path="/edit-customer/:id" element={<AddCustomerComponent/>}></Route>
+      <Route path = "/devices" element = {<ListDeviceComponent/>}></Route>
+      <Route path = "/add-device" element = {<AddDeviceComponent/>} ></Route>
+      <Route path="/edit-device/:id" element={<AddDeviceComponent/>}></Route>
+      
+      </Routes>
+    
+ 
+    <FooterComponent/>
+    </Router>
+   </div>
   );
 }
 
 export default App;
+
