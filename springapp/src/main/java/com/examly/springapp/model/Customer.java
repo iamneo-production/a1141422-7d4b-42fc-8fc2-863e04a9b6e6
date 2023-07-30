f8275da38be17b205e58cf5cdabaee4a6437cba5
Mirudhula123 <mirudhula.e.2019.cse@rajalakshmi.edu.in>
2023-07-30 16:07:50 +0000

    commit by mirudhula backend testcases

---
+++ springapp/src/main/java/com/examly/springapp/model/Customer.java
@@ -0,0 +1,29 @@
+package com.examly.springapp.model;
+
+
+import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customers",uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class Customer {
+
+    @Id
+     @Column(name = "id")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "phone")
+    private String phone;
+}
+
