458305bb1a841701a5c1eb34a940dfb3024dc1ac
Sree-Deekshitha <sreedeekshitha.999@gmail.com>
2023-07-30 13:36:26 +0000

    commit by sree

---
+++ springapp/src/main/java/com/examly/springapp/service/EmployeeRepairMappingService.java
@@ -0,0 +1,20 @@
+package com.examly.springapp.service;
+import com.examly.springapp.model.EmployeeRepairMapping;
+import com.examly.springapp.repository.EmployeeRepairMappingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
+public class EmployeeRepairMappingService {
+    @Autowired
+    private EmployeeRepairMappingRepository employeeRepairMappingRepository;
+    public List<EmployeeRepairMapping> getAllEmployeeMappings() {
+        return employeeRepairMappingRepository.findAll();
+    }
+    public EmployeeRepairMapping assignRepairIdToEmployee(Long employeeId, EmployeeRepairMapping em) {
+        EmployeeRepairMapping mapping = new EmployeeRepairMapping();
+        mapping.setEmployeeId(employeeId);
+        mapping.setRepairId(em.getRepairId());
+        return employeeRepairMappingRepository.save(mapping);
+    }
+}
