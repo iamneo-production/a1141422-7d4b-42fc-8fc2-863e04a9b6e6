968ef14551905805156b1830b9183006484b9ca8
AvinashD31 <sit19cs100@sairamtap.edu.in>
2023-07-31 12:44:08 +0000

    removed unused imports from customerbooking rendered

--- reactapp/src/components/CustomerBooking/Customer_bking_rendered.js
+++ reactapp/src/components/CustomerBooking/Customer_bking_rendered.js
@@ -1,8 +1,6 @@
 import React from 'react';
 //import './App.css';
 import NavBar from './NavBar';
-import {Container, Row, Col} from "reactstrap";
-import MenuContent from './MenuContent';
 import Custmr_card from './Custmr_card';
 
 export default function Customer_bking_rendered() {
