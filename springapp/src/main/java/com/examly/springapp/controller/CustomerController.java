3dfd3e00cdd15fe9d603501ac237a192ad0592af
hi <95462800+Sree-Deekshitha@users.noreply.github.com>
2023-07-31 22:38:20 +0530

    Update CustomerController.java

--- springapp/src/main/java/com/examly/springapp/controller/CustomerController.java
+++ springapp/src/main/java/com/examly/springapp/controller/CustomerController.java
@@ -35,6 +35,11 @@ public class CustomerController {
         Customer customer = customerService.getCustomerById(id);
         return ResponseEntity.ok(customer);
     }
+    @GetMapping("/email/{email}")
+    public ResponseEntity<Customer> getCustomerByEmail(@PathVariable String email) {
+        Customer customer = customerService.getCustomerByEmail(email);
+        return ResponseEntity.ok(customer);
+    }
 
  @PutMapping("{id}")
     public ResponseEntity<Customer> updateCustomer(@PathVariable long id, @RequestBody Customer customerDetails) {
