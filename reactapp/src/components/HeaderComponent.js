fc63d5bb4da979bd432838498dce855ac270f350
mylariprasad <lakshmiprasadmylari@gmail.com>
2023-07-31 11:50:58 +0000

    commit

--- reactapp/src/components/HeaderComponent.js
+++ reactapp/src/components/HeaderComponent.js
@@ -4,13 +4,13 @@ import Sidebar from './Sidebar';
 import { Link } from 'react-router-dom';
 const HeaderComponent = () => {
   return (
-    <div className='row top_header' style={{display:'flex'}}>
+    <div className='row top_header' >
       <header>
-        <nav className='navbar navbar-expand-md'>
-            <div style={{textAlign : 'center', height : '50px', width : '110%'}}>
+        <div className='navbar navbar-expand-md'>
+            <div style={{textAlign : 'center', height : '50px', width : '100vw', display:'flex'}}>
             <h1> AC REPAIR PORTAL</h1>
-            </div>
-            <div className='col-1 col-md-1 col-xl-1 col-lg-1'>
+            
+            <div className='col-1 col-md-1 col-xl-1 col-lg-1' style={{margin:'40px', marginLeft:"600px"}}>
         <div className="container-Project-item" >
         <Link className="menu-item" to={`/AboutUs`}>
         <buton type="button" className="btn btn-primary">About Us</buton>
@@ -18,14 +18,15 @@ const HeaderComponent = () => {
            
         </div>
       </div>
-      <div className='col-1 col-md-1 col-xl-1 col-lg-1'>
+      <div className='col-1 col-md-1 col-xl-1 col-lg-1' style={{margin:'40px', marginLeft:"50px"}}>
         <div className="container-Project-item" >
         <Link to="/AboutUs#contact"> 
         <button  type="button" className="btn btn-primary">Contact Us</button>
         </Link>
         </div>
       </div>
-        </nav>
+      </div>
+        </div>
       </header>
     </div>
   )
